3e25586b19b1b3be163db01b0a7faae4a0d435a7
Wizari <wizaripost@gmail.com>
2026-01-15 14:47:04 +0300

    first commit

---
+++ src/main/kotlin/com/gmail/wizaripost/json_converter/JsonConverterApplication.kt
@@ -0,0 +1,11 @@
+package com.gmail.wizaripost.json_converter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JsonConverterApplication
+
+fun main(args: Array<String>) {
+	runApplication<JsonConverterApplication>(*args)
+}
---
+++ src/test/kotlin/com/gmail/wizaripost/json_converter/JsonConverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gmail.wizaripost.json_converter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JsonConverterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
